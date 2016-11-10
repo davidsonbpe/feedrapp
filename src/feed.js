f214211e952806d1a44043047201e63024d239e5
Gino <ginocote@gmail.com>
2016-11-10 15:57:33 -0500

    item.enclosure

--- src/feed.js
+++ src/feed.js
@@ -77,7 +77,7 @@ export default class Feed {
       categories: item.categories || [],
       author: item.author || this._extractCreator(item) || author,
       thumbnail: this._extractThumbnail(item),
-      extensions: [Extension]
+      enclosure: { url: item.enclosure } // element attributes
     };
   }
 
