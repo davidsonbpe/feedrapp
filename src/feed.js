969ba9ee431d679cc3c1dfebead56cd7ab0b7133
Gino <ginocote@gmail.com>
2016-11-10 12:45:08 -0500

    res.data

--- src/feed.js
+++ src/feed.js
@@ -38,7 +38,7 @@ export default class Feed {
     let author = data.author || '';
 
     return {
-      data: feed,
+      data: res.data,
       feedUrl: this.url,
       title: data.title,
       link: data.link,
